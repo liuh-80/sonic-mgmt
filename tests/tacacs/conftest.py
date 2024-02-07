8fc5d02ce6463f2eb7c3c49169bd7e33aace83d7
liuh-80 <liuh@microsoft.com>
2024-02-07 09:31:05 +0000

    Fix UT issue

--- tests/tacacs/conftest.py
+++ tests/tacacs/conftest.py
@@ -12,7 +12,8 @@ def check_tacacs(ptfhost, duthosts, enum_rand_one_per_hwsku_hostname, tacacs_cre
     print_tacacs_creds(tacacs_creds)
     duthost = duthosts[enum_rand_one_per_hwsku_hostname]
     tacacs_server_ip = ptfhost.mgmt_ip
-    setup_tacacs_client(duthost, tacacs_creds, tacacs_server_ip)
+    tacacs_server_passkey = tacacs_creds[duthost.hostname]['tacacs_passkey']
+    setup_tacacs_client(duthost, tacacs_creds, tacacs_server_ip, tacacs_server_passkey)
     setup_tacacs_server(ptfhost, tacacs_creds, duthost)
 
     yield
@@ -28,7 +29,8 @@ def check_tacacs_v6(ptfhost, duthosts, enum_rand_one_per_hwsku_hostname, tacacs_
     if 'ansible_hostv6' not in ptfhost_vars:
         pytest.skip("Skip IPv6 test. ptf ansible_hostv6 not configured.")
     tacacs_server_ip = ptfhost_vars['ansible_hostv6']
-    setup_tacacs_client(duthost, tacacs_creds, tacacs_server_ip)
+    tacacs_server_passkey = tacacs_creds[duthost.hostname]['tacacs_passkey']
+    setup_tacacs_client(duthost, tacacs_creds, tacacs_server_ip, tacacs_server_passkey)
     setup_tacacs_server(ptfhost, tacacs_creds, duthost)
 
     yield
